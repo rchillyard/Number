9554172da0a2b20d5aecc60205dd732120e07127
Robin Hillyard <scalaprof@gmail.com>
2020-12-10 21:50:38 -0500

    Improving Fuzz
    Improved definitions of Relative and AbsoluteFuzz;
    Valuable: new type constructor.

--- src/main/scala/com/phasmidsoftware/number/core/Fuzz.scala
+++ src/main/scala/com/phasmidsoftware/number/core/Fuzz.scala
@@ -1,5 +1,9 @@
 package com.phasmidsoftware.number.core
 
+import scala.math.Numeric.DoubleIsFractional
+import scala.math.Ordering
+import scala.util.Try
+
 /**
   * Type constructor which will give fuzzy behavior to a type T.
   *
@@ -9,12 +13,31 @@ trait Fuzz[T] {
   val shape: Shape
 }
 
-case class RelativeFuzz[T](tolerance: Double, shape: Shape) extends Fuzz[T] {
-  //  def magnitude(t: T): T
+case class RelativeFuzz[T: Valuable](tolerance: Double, shape: Shape) extends Fuzz[T] {
+  /**
+    * This method takes a value of T on which to base a relative fuzz value.
+    *
+    * @param t the nominal value of the fuzzy number.
+    * @return an optional RelativeFuzz[T]
+    */
+  def absolute(t: T): Option[AbsoluteFuzz[T]] = {
+    val tf = implicitly[Valuable[T]]
+    Try(AbsoluteFuzz(tf.times(tf.fromDouble(tolerance), t), shape)).toOption
+  }
 }
 
-case class AbsoluteFuzz[T](magnitude: T, shape: Shape) extends Fuzz[T] {
-  //  def tolerance(t: T): Double
+case class AbsoluteFuzz[T: Valuable](magnitude: T, shape: Shape) extends Fuzz[T] {
+  /**
+    * This method takes a value of T on which to base a relative fuzz value.
+    * NOTE: if t is zero, we will return None, which corresponds to an Exact number.
+    *
+    * @param t the nominal value of the fuzzy number.
+    * @return an optional RelativeFuzz[T]
+    */
+  def relative(t: T): Option[RelativeFuzz[T]] = {
+    val tf = implicitly[Fractional[T]]
+    Try(RelativeFuzz(tf.toDouble(tf.div(magnitude, t)), shape)).toOption
+  }
 }
 
 trait Shape
@@ -25,4 +48,18 @@ case object Gaussian extends Shape
 
 trait Fuzzy[T] {
   val fuzz: Option[Fuzz[T]]
+}
+
+trait Valuable[T] extends Fractional[T] {
+  def fromDouble(x: Double): T
+}
+
+trait ValuableDouble extends Valuable[Double] with DoubleIsFractional with Ordering.Double.IeeeOrdering {
+  override def fromDouble(x: Double): Double = x
+}
+
+object Valuable {
+
+  implicit object ValuableDouble extends ValuableDouble
+
 }
