f98309d432cb158579d8afd5c74eae3f9d42befe
Robin Hillyard <r.hillyard@northeastern.edu>
2021-08-04 20:55:20 -0400

    Finalized the Foucault calculations
    FuzzyNumberSpec, Foucault1.sc and Foucault2.sc are now tested, accurate and all set!

--- src/test/scala/com/phasmidsoftware/number/core/FuzzyNumberSpec.scala
+++ src/test/scala/com/phasmidsoftware/number/core/FuzzyNumberSpec.scala
@@ -480,7 +480,7 @@ class FuzzyNumberSpec extends AnyFlatSpec with should.Matchers {
   }
 
   it should "calculate length of Foucault's pendulum but with relative box fuzziness" in {
-    // NOTE this is here to support Foucault.sc
+    // NOTE this is here to support Foucault1.sc
     val g = Number("9.81*")
     println(s"g fuzz: ${g.fuzz.map(_.normalize(9.81, relative = true))}")
     val t = Number("16.5*")
@@ -491,33 +491,32 @@ class FuzzyNumberSpec extends AnyFlatSpec with should.Matchers {
     val square: Option[Number] = tScaled.flatMap(x => (x ^ 2).asNumber)
     println(s"square fuzz: ${square.get.fuzz}")
 
-    // FIXME Issue #52 the following number is not correct: it should be approximately 0.006 (twice the value shown above for t)
+    // NOTE Relative error should be approximately 0.006 (twice the value shown above for t)
     square.get.fuzz should matchPattern { case Some(RelativeFuzz(_, Box)) => }
     square.get.fuzz.get.asInstanceOf[RelativeFuzz[Double]].tolerance shouldBe 0.006 +- 0.001
 
     val length: Option[Number] = square flatMap (x => (x * g).asNumber)
-    // NOTE the following number is not correct: it should be approximately 0.0065
     length.get.fuzz shouldBe Some(RelativeFuzz(0.0065702900565341804, Box))
 
     println(length.get)
   }
   it should "calculate length of Foucault's pendulum" in {
-    // NOTE this is here to support Foucault.sc
+    // NOTE this is here to support Foucault2.sc
     val g = Number("9.81*")
-    println(g.fuzz)
-    val t = Number("16.5(2)")
-    println(t.fuzz)
+    println(s"g fuzz: ${g.fuzz.map(_.normalize(9.81, relative = true))}")
+    val t = Number("16.487(41)")
+    println(s"t fuzz: ${t.fuzz.map(_.normalize(16.5, relative = true))}")
 
-    val tScaled: Option[Number] = (t / Number.twoPi).asNumber
-    println(tScaled.get.fuzz)
+    val tScaled: Option[Number] = (t / twoPi).asNumber
+    println(s"tScaled fuzz: ${tScaled.get.fuzz}")
     val square: Option[Number] = tScaled.flatMap(x => (x ^ 2).asNumber)
-    println(square.get.fuzz)
+    println(s"square fuzz: ${square.get.fuzz}")
 
-    // NOTE the following value is inconsistent with Foucault.sc
-    square.get.fuzz shouldBe Some(RelativeFuzz(0.024242424242426956, Gaussian))
+    square.get.fuzz should matchPattern { case Some(RelativeFuzz(_, Gaussian)) => }
+    square.get.fuzz.get.asInstanceOf[RelativeFuzz[Double]].tolerance shouldBe 0.005 +- 0.0002
 
     val length: Option[Number] = square flatMap (x => (x * g).asNumber)
-    length.get.fuzz shouldBe Some(RelativeFuzz(0.024390888700791504, Gaussian))
+    length.get.fuzz shouldBe Some(RelativeFuzz(0.005139383648321391, Gaussian))
 
     println(length.get)
   }
