a58725a5761b417f8838afea101fcd5f42d45093
Robin Hillyard <r.hillyard@northeastern.edu>
2023-06-30 21:11:38 -0400

    Added ExpressionJ
    Also added (java) ExpressionTest and ComplexTest.

---
+++ src/main/scala/com/phasmidsoftware/number/java/ExpressionJ.scala
@@ -0,0 +1,12 @@
+package com.phasmidsoftware.number.java
+
+import com.phasmidsoftware.number.core.Expression
+
+object ExpressionJ {
+
+    import com.phasmidsoftware.number.core.Expression.ExpressionOps
+
+    def add(x: Expression, y: Expression): Expression = x + y
+
+    def multiply(x: Expression, y: Expression): Expression = x * y
+}
