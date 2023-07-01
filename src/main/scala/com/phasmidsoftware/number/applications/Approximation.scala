7b1fe34fbb56e9832c68e1026539938c61b81a8f
Robin Hillyard <r.hillyard@northeastern.edu>
2023-07-01 13:39:26 -0400

    Implemented Halley's method
    Approximation:
    * added Halley's method;
    * added a parameter to evaluate method: fixed a little problem such that precision was being lost.

--- src/main/scala/com/phasmidsoftware/number/applications/Approximation.scala
+++ src/main/scala/com/phasmidsoftware/number/applications/Approximation.scala
@@ -2,9 +2,9 @@ package com.phasmidsoftware.number.applications
 
 import com.phasmidsoftware.number.core.Field.convertToNumber
 import com.phasmidsoftware.number.core.FuzzyNumber.NumberIsFuzzy
-import com.phasmidsoftware.number.core.Number
-
+import com.phasmidsoftware.number.core.{Field, Number}
 import scala.annotation.tailrec
+import scala.math.Fractional.Implicits.infixFractionalOps
 import scala.util.{Failure, Success, Try}
 
 /**
@@ -13,7 +13,7 @@ import scala.util.{Failure, Success, Try}
 object Approximation {
 
   /**
-    * Method used by solve to iterate towards the the solution.
+    * Method used by solve to iterate towards a solution.
     * This method of iterating towards a root of a continuously differentiable function implements Householder's method in general.
     * The number of functions provided is the "order" of the method.
     * The Newton-Raphson method is Householder's method, order 1.
@@ -23,40 +23,52 @@ object Approximation {
     * @param functions the derivatives of the function, starting with the 0th, i.e. the function itself.
     */
   def iterate(functions: (Double => Double)*): Number => Try[Number] = {
-    // NOTE: for now, we ignore anything beyond the first derivative, i.e. we just implement Newton.
     x =>
-      println(x)
-      require(functions.size > 1, "iterate: insufficient functions provided")
       val f = functions(0)
       val dfByDx = functions(1)
       for {
-        p <- evaluate(f)(x)
-        q <- evaluate(dfByDx)(x)
+        p <- evaluate(f, dfByDx)(x)
+//        _ = println(s"iterate: x=$x; f(x)=$p")
+        q <- evaluateWithoutDerivative(dfByDx)(x)
         r = p.divide(q)
-        t = x.doSubtract(convertToNumber(r))
+        s <- correction(r, q, x, functions)
+        t = x.doSubtract(convertToNumber(s))
       } yield t
   }
 
   /**
-    * Method to determine of the approximation can be considered converged.
+    * Method to determine if the approximation can be considered converged.
+    * We use the convolution of the probability density functions (PDFs) to determine if the probability of overlap exceeds the value p.
     *
-    * @param f the function.
-    * @param p a probability between 0 and 1 -- 0 would always result in true; 1 will result in false unless x1 actually is x2.
-    * @param x a Number which will be compared with zero.
+    * @param f      the function.
+    * @param dfByDx the derivative function.
+    * @param p      a probability between 0 and 1 -- 0 would always result in true; 1 will result in false unless x1 actually is x2.
+    * @param x      a Number which will be compared with zero.
     * @return a Try[Boolean].
     */
-  def converged(f: Double => Double)(p: Double)(x: Number): Try[Boolean] =
-    evaluate(f)(x) map (n => NumberIsFuzzy.same(p)(n, Number.zero))
+  def converged(f: Double => Double, dfByDx: Double => Double)(p: Double)(x: Number): Try[Boolean] =
+    evaluate(f, dfByDx)(x) map (n => NumberIsFuzzy.same(p)(n, Number.zero))
 
   /**
     * Method to evaluate function f at value x.
     * We ignore the derivative of the function at x.
+    * NOTE: this has consequences in terms of the precision of the result.
     *
     * @param f the function.
     * @param x the value to pass into f.
     * @return the function's value at x.
     */
-  def evaluate(f: Double => Double)(x: Number): Try[Number] = x.applyFunc(f, _ => 0)
+  def evaluateWithoutDerivative(f: Double => Double)(x: Number): Try[Number] = evaluate(f, _ => 0)(x)
+
+  /**
+    * Method to evaluate function f at value x.
+    *
+    * @param f the function.
+    * @param dfByDx the derivative of the function.
+    * @param x the value to pass into f.
+    * @return the function's value at x.
+    */
+  def evaluate(f: Double => Double, dfByDx: Double => Double)(x: Number): Try[Number] = x.applyFunc(f, dfByDx)
 
   /**
     * This method of finding a root of a continuously differentiable function implements Householder's method in general.
@@ -71,8 +83,10 @@ object Approximation {
     * @param x0          the initial guess.
     */
   def solve(probability: Double, functions: (Double => Double)*)(x0: Number): Try[Number] = {
-    val tester: Number => Try[Boolean] = converged(functions(0))(probability)
-    val iterator = iterate(functions: _*)
+    require(functions.size > 1, "solve: insufficient functions provided")
+
+    val tester: Number => Try[Boolean] = converged(functions(0), functions(1))(probability)
+    val iterator: Number => Try[Number] = iterate(functions: _*)
 
     @tailrec
     def inner(xy: Try[Number]): Try[Number] = xy match {
@@ -85,5 +99,28 @@ object Approximation {
     }
 
     inner(Success(x0))
+  }
+
+  /**
+    * This method evaluates the correction for higher derivatives.
+    * NOTE only the Halley version is currently implemented (where n = 3).
+    *
+    * @param r the ratio of f(x) to f'(x).
+    * @param q the value of f'(x).
+    * @param x the value of x.
+    * @param f the list of derivate functions: f, f', f&#39;&#39;, etc.
+    * @return the correction term.
+    */
+  private def correction(r: Field, q: Field, x: Number, f: Seq[Double => Double]): Try[Number] = f.length match {
+    case n if n < 3 =>
+      Success(convertToNumber(r))
+    case 3 => // Halley's method
+      for {
+        y <- evaluateWithoutDerivative(f(2))(x)
+        z = y divide (r * Number.two)
+        w = r.divide(Number.one - convertToNumber(r * convertToNumber(z)))
+      } yield convertToNumber(w)
+    case n =>
+      Failure(com.phasmidsoftware.number.core.NumberException(s"Approximation.iterate: does not implement correction with $n functions"))
   }
 }
--- src/test/scala/com/phasmidsoftware/number/applications/ApproximationSpec.scala
+++ src/test/scala/com/phasmidsoftware/number/applications/ApproximationSpec.scala
@@ -1,17 +1,23 @@
 package com.phasmidsoftware.number.applications
 
+import com.phasmidsoftware.number.applications.Approximation.evaluateWithoutDerivative
 import com.phasmidsoftware.number.core.Number
+import org.scalatest.PrivateMethodTester
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
-
 import scala.util.{Success, Try}
 
-class ApproximationSpec extends AnyFlatSpec with should.Matchers {
+class ApproximationSpec extends AnyFlatSpec with should.Matchers with PrivateMethodTester {
 
-  behavior of "ApproximationSpec"
+  behavior of "Approximation"
 
   val newtonsPolynomial: Double => Double = x => x * x * x + 6 * x * x + 10 * x - 1
   val newtonsDerivative: Double => Double = x => 3 * x * x + 12 * x + 10
+  val newtonsSecondDerivative: Double => Double = x => 6 * x + 12
+
+  val cosineFunction: Double => Double = x => math.cos(x) - x
+  val cosineDerivative: Double => Double = x => -math.sin(x) - 1
+  val cosineSecondDerivative: Double => Double = x => -math.cos(x)
 
   it should "solve Newton's original problem" in {
     val result = Approximation.solve(0.9,
@@ -21,9 +27,35 @@ class ApproximationSpec extends AnyFlatSpec with should.Matchers {
     result.isSuccess shouldBe true
   }
 
+  it should "solve cosine problem" in {
+    val result = Approximation.solve(0.9,
+      cosineFunction,
+      cosineDerivative
+    )(Number.half)
+    result.isSuccess shouldBe true
+  }
+
+  it should "solve cosine problem using Halley's method" in {
+    val result = Approximation.solve(0.9,
+      cosineFunction,
+      cosineDerivative,
+      cosineSecondDerivative
+    )(Number.half)
+    result.isSuccess shouldBe true
+  }
+
+  it should "solve Newton's original problem using Halley's method" in {
+    val result = Approximation.solve(0.9,
+      newtonsPolynomial,
+      newtonsDerivative,
+      newtonsSecondDerivative,
+    )(Number.zero)
+    result.isSuccess shouldBe true
+  }
+
   it should "solve the inverse square root problem for 0.15625" in {
     val x = 0.15625
-    val inverseSquareRoot: Double => Double = y => y * y - 1.0/x
+    val inverseSquareRoot: Double => Double = y => y * y - 1.0 / x
     val inverseDerivative: Double => Double = y => 2 * y
 
     Approximation.solve(0.9, inverseSquareRoot, inverseDerivative)(Number(2.5)).isSuccess shouldBe true
@@ -41,6 +73,12 @@ class ApproximationSpec extends AnyFlatSpec with should.Matchers {
     newtonsDerivative(0.09455) shouldBe 11.1614191075
   }
 
+  it should "evaluate newtonsSecondDerivative" in {
+    newtonsSecondDerivative(0.1) shouldBe 12.6
+    newtonsSecondDerivative(0.094) shouldBe 12.564
+    newtonsSecondDerivative(0.09455) shouldBe 12.5673
+  }
+
   it should "evaluate newtons method" in {
     0.1 - newtonsPolynomial(0.1) / newtonsDerivative(0.1) shouldBe 0.09456812110418523
     0.094 - newtonsPolynomial(0.094) / newtonsDerivative(0.094) shouldBe 0.09455165283847572
@@ -48,7 +86,7 @@ class ApproximationSpec extends AnyFlatSpec with should.Matchers {
   }
 
   it should "converged" in {
-    Approximation.converged(newtonsPolynomial)(0.9)(Number(0.0945514815423266)) shouldBe Success(true)
+    Approximation.converged(newtonsPolynomial, newtonsDerivative)(0.9)(Number(0.0945514815423266)) shouldBe Success(true)
   }
 
   it should "iterate" in {
@@ -59,10 +97,36 @@ class ApproximationSpec extends AnyFlatSpec with should.Matchers {
   }
 
   it should "delta" in {
-    val zy = Approximation.evaluate(newtonsPolynomial)(Number(0.09456812110))
+    val zy = Approximation.evaluate(newtonsPolynomial, newtonsDerivative)(Number(0.09456812110))
     println(zy)
     zy.isSuccess shouldBe true
 
+  }
+
+  it should "do correction for Newton's polynomial" in {
+    val correctionSymbol = Symbol("correction")
+    val z = PrivateMethod[Try[Number]](correctionSymbol)
+    val x = Number(0.1)
+    val py = evaluateWithoutDerivative(newtonsPolynomial)(x) // x^3 + 6x^2 + 10x - 1
+    val qy = evaluateWithoutDerivative(newtonsDerivative)(x) // 3x^2 + 12x + 10
+    val ry = for (p <- py; q <- qy) yield p divide q
+    val cy: Try[Number] = Approximation invokePrivate z(ry.get, qy.get, x, Seq(newtonsPolynomial, newtonsDerivative, newtonsSecondDerivative))
+    cy.isSuccess shouldBe true
+    cy.get.toDouble.isDefined shouldBe true
+    cy.get.toDouble.get shouldBe -0.001024882810531 +- 1E-7
+  }
+
+  it should "do correction for cosine problem" in {
+    val correctionSymbol = Symbol("correction")
+    val z = PrivateMethod[Try[Number]](correctionSymbol)
+    val x = Number.half
+    val py = evaluateWithoutDerivative(cosineFunction)(x) // cos(x) - x
+    val qy = evaluateWithoutDerivative(cosineDerivative)(x) // -sin(x) - 1
+    val ry = for (p <- py; q <- qy) yield p divide q
+    val cy: Try[Number] = Approximation invokePrivate z(ry.get, qy.get, x, Seq(cosineFunction, cosineDerivative, cosineSecondDerivative))
+    cy.isSuccess shouldBe true
+    cy.get.toDouble.isDefined shouldBe true
+    cy.get.toDouble.get shouldBe -0.177386686 +- 1E-7
   }
 
 }
