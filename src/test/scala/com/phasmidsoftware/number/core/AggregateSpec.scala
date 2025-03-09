d558b36a52c0f9f22bd053f06783309f88486426
Robin Hillyard <r.hillyard@northeastern.edu>
2025-03-08 20:26:41 -0500

    Missing from previous commit

--- src/test/scala/com/phasmidsoftware/number/core/AggregateSpec.scala
+++ src/test/scala/com/phasmidsoftware/number/core/AggregateSpec.scala
@@ -8,47 +8,47 @@ class AggregateSpec extends AnyFlatSpec with should.Matchers {
   behavior of "Aggregate"
 
   it should "isExactInContext" in {
-    val target = Aggregate(Constants.one, Constants.one)
+    val target = Aggregate.total(Constants.one, Constants.one)
     target.isExactInContext(Some(PureNumber)) shouldBe true
   }
 
   it should "context" in {
-    val target = Aggregate(Constants.one, Constants.one)
+    val target = Aggregate.total(Constants.one, Constants.one)
     target.context shouldBe Some(PureNumber)
   }
 
   it should "render" in {
-    val target = Aggregate(Constants.one, Constants.one)
+    val target = Aggregate.total(Constants.one, Constants.one)
     target.toString shouldBe "1+1"
     target.render shouldBe "2"
   }
 
   it should "depth" in {
-    val target = Aggregate(Constants.one, Constants.one)
+    val target = Aggregate.total(Constants.one, Constants.one)
     target.depth shouldBe 2
   }
 
   it should "evaluate 1" in {
-    val target = Aggregate(Constants.one, Constants.one)
-    target.evaluate(None) shouldBe Constants.two
+    val target = Aggregate.total(Constants.one, Constants.one)
+    target.evaluate shouldBe Constants.two
   }
 
   it should "evaluate 2" in {
-    val target = Aggregate(Constants.one, Constants.two, Constants.minusOne)
-    target.evaluate(None) shouldBe Constants.two
+    val target = Aggregate.total(Constants.one, Constants.two, Constants.minusOne)
+    target.evaluate shouldBe Constants.two
   }
 
   it should "evaluate 3" in {
     import com.phasmidsoftware.number.core.Expression.ExpressionOps
-    val target = Aggregate(One * MinusOne, Two + One, MinusOne * 5, Constants.two)
-    target.evaluate(None) shouldBe Constants.minusOne
+    val target = Aggregate.total(One * MinusOne, Two + One, MinusOne * 5, Constants.two)
+    target.evaluate shouldBe Constants.minusOne
   }
 
   val em: ExpressionMatchers = Expression.em
 
   it should "simplify 1" in {
     import com.phasmidsoftware.number.core.Expression.ExpressionOps
-    val target = Aggregate(Two * MinusOne, Two + One, MinusOne * 5, Constants.two)
+    val target = Aggregate.total(Two * MinusOne, Two + One, MinusOne * 5, Constants.two)
     val result = em.simplifier(target)
     result shouldBe em.Match(Aggregate(Sum, List(Literal(3), Literal(-5))))
     val value1 = result flatMap (em.evaluator(None))
