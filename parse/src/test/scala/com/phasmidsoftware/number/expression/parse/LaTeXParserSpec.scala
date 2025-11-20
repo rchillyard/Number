c27336b4170457ff9c6365b5e14faba132675332
Robin Hillyard <r.hillyard@northeastern.edu>
2025-11-20 17:00:56 -0500

    Added additional test of evaluated expression
    parse test status 5/0/0

--- parse/src/test/scala/com/phasmidsoftware/number/expression/parse/LaTeXParserSpec.scala
+++ parse/src/test/scala/com/phasmidsoftware/number/expression/parse/LaTeXParserSpec.scala
@@ -19,17 +19,21 @@ class LaTeXParserSpec extends AnyFlatSpec with should.Matchers {
   val p: String => Parsed[MathExpr] = LaTeXParser.parse
 
   it should "apply number expressions" in {
-    p.apply("1") shouldBe Parsed.Success(Literal(WholeNumber(1), Some("1")), 1)
-    p.apply("1+2") shouldBe Parsed.Success(BiFunction(Literal(WholeNumber(1), Some("1")), Literal(WholeNumber(2), Some("2")), Sum), 3)
+    p.apply("1") shouldBe Parsed.Success(One, 1)
+    p.apply("1+2") shouldBe Parsed.Success(BiFunction(One, Two, Sum), 3)
     p.apply("6*7") shouldBe Parsed.Success(BiFunction(Literal(WholeNumber(6), Some("6")), Literal(WholeNumber(7), Some("7")), Product), 3)
     p.apply("42/7") shouldBe Parsed.Success(BiFunction(Literal(WholeNumber(42), Some("42")), UniFunction(Literal(WholeNumber(7), Some("7")), Reciprocal), Product), 4)
     p.apply("6*(3+4)") shouldBe Parsed.Success(BiFunction(Literal(WholeNumber(6), Some("6")), BiFunction(Literal(WholeNumber(3), Some("3")), Literal(WholeNumber(4), Some("4")), Sum), Product), 7)
     p.apply("""\frac{42}{6}""") shouldBe Parsed.Success(BiFunction(Literal(WholeNumber(42), Some("42")), UniFunction(Literal(WholeNumber(6), Some("6")), Reciprocal), Product), 12)
+    p.apply("3^2") shouldBe Parsed.Success(BiFunction(Literal(WholeNumber(3), Some("3")), Literal(WholeNumber(2), Some("2")), Power), 3)
+    p.apply("2+3*4") shouldBe Parsed.Success(BiFunction(Two, BiFunction(Literal(WholeNumber(3),Some("3")), Literal(WholeNumber(4),Some("4")), Product), Sum), 5)
+    p.apply("3^2+4") shouldBe Parsed.Success(BiFunction(BiFunction(Literal(WholeNumber(3), Some("3")), Literal(WholeNumber(2), Some("2")), Power), Literal(WholeNumber(4), Some("4")), Sum), 5)
+    p.apply("3^2+4*5") shouldBe Parsed.Success(BiFunction(BiFunction(Literal(WholeNumber(3), Some("3")), Literal(WholeNumber(2), Some("2")), Power), BiFunction(Literal(WholeNumber(4), Some("4")), Literal(WholeNumber(5), Some("5")), Product), Sum), 7)
   }
   it should "apply pi" in {
     p.apply("""\pi""") shouldBe Parsed.Success(ConstPi, 3)
-    p.apply("""2\pi""") shouldBe Parsed.Success(BiFunction(Literal(WholeNumber(2), Some("2")), ConstPi, Product), 4)
-    p.apply("""2*\pi""") shouldBe Parsed.Success(BiFunction(Literal(WholeNumber(2), Some("2")), ConstPi, Product), 5)
+    p.apply("""2\pi""") shouldBe Parsed.Success(BiFunction(Two, ConstPi, Product), 4)
+    p.apply("""2*\pi""") shouldBe Parsed.Success(BiFunction(Two, ConstPi, Product), 5)
   }
   it should "apply e" in {
     p.apply("""\e""") shouldBe Parsed.Success(ConstE, 2)
@@ -42,4 +46,12 @@ class LaTeXParserSpec extends AnyFlatSpec with should.Matchers {
     p.apply("""\ln(\e)""") shouldBe Parsed.Success(UniFunction(ConstE, Ln), 7)
 //    p.apply("""\{exp}(2)""") shouldBe Parsed.Success(UniFunction(One,Exp), 7)
   }
+
+  behavior of "parsed expressions"
+  it should "apply number expressions" in {
+    val parsed = p.apply("2+3*4")
+    parsed.isSuccess shouldBe true
+    parsed.get.value.evaluateAsIs shouldBe Some(WholeNumber(14))
+  }
+
 }
